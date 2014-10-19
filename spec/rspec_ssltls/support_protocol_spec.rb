c2402c6ab29ba49f8759c694528ad910b280ad9a
OTA Hiroshi <otahi.pub@gmail.com>
2014-10-19 09:25:06 +0900

    Replace SSLv2 to SSLv3 to avoid invalid protocol error

--- spec/rspec_ssltls/support_protocol_spec.rb
+++ spec/rspec_ssltls/support_protocol_spec.rb
@@ -24,13 +24,13 @@ end
 describe 'rspec-ssltls matchers' do
   describe '#support_protocol' do
     it 'can evalutate support protocol' do
-      expect('www.example.com:443').not_to support_protocol('SSLv2')
+      expect('www.example.com:443').not_to support_protocol('SSLv3')
       stub_ssl_socket(ssl_version: 'TLSv1')
       expect('www.example.com:443').to support_protocol('TLSv1')
       expect('www.example.com:443').to support_protocol(:TLSv1)
       stub_ssl_socket(ssl_version: nil)
-      expect('www.example.com:443').not_to support_protocol('SSLv2')
-      expect('www.example.com:443').not_to support_protocol([:TLSv1, 'SSLv2'])
+      expect('www.example.com:443').not_to support_protocol('SSLv3')
+      expect('www.example.com:443').not_to support_protocol([:TLSv1, 'SSLv3'])
     end
   end
 end
