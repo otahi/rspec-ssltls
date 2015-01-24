cfabed38abe7cc0bf1e355fbbecead35306a5c40
Hiroshi Ota <otahi.pub@gmail.com>
2015-01-25 08:10:43 +0900

    Add RspecSsltls::Util.open_socket able to use proxy

--- lib/rspec_ssltls/util.rb
+++ lib/rspec_ssltls/util.rb
@@ -1,3 +1,5 @@
+require 'net/ssh/proxy/http'
+
 # Easily test your SSL/TLS with RSpec.
 module RspecSsltls
   # Utility class
@@ -17,5 +19,29 @@ module RspecSsltls
           OpenSSL::SSL::SSLContext::METHODS.map { |a| a.to_s })
       invalid_protocol.size > 0 ? invalid_protocol : nil
     end
+
+    def self.open_socket(uri, options = {})
+      if options[:proxy]
+        proxy_uri = build_uri(options[:proxy])
+        proxy_server = Net::SSH::Proxy::HTTP.new(proxy_uri.host,
+                                                 proxy_uri.host,
+                                                 user: proxy_uri.user,
+                                                 password: proxy_uri.password)
+        proxy_server.open(uri.host, uri.port)
+      else
+        TCPSocket.open(uri.host, uri.port)
+      end
+    end
+
+    def self.build_uri(source)
+      if source.is_a?(String)
+        source = 'http://' + source unless source.start_with?('http://')
+        URI.parse(source)
+      else
+        source
+      end
+    end
+
+    private_class_method :build_uri
   end
 end
